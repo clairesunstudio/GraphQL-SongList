0276b973d8eadd32716fd26e278c0940fefa04dd
Minghua Sun <clairesunstudio@gmail.com>
2017-12-21 15:55:21 -0500

    Link songlist to songcreate pages

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Link, hashHistory } from 'react-router'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -14,11 +15,17 @@ class SongCreate extends Component {
       variables: {
         title: this.state.title
       }
-    })
+    }).then(() => hashHistory.push('/'))
   }
   render() {
     return (
       <div>
+        <Link
+          to="/"
+          className="btn-floating btn-large blue"
+        >
+          <i className="material-icons">keyboard_backspace</i>
+        </Link>
         <h3>Create a new song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title: </label>
